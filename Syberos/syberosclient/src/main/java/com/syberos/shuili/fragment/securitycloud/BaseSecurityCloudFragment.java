fc8caebf96a1bfac1d7c146142680660999bbda2
xiaobei56 <xiaobei_63@163.com>
2018-07-24 18:51:56 +0800

    add "关键指标"

--- Syberos/syberosclient/src/main/java/com/syberos/shuili/fragment/securitycloud/BaseSecurityCloudFragment.java
+++ Syberos/syberosclient/src/main/java/com/syberos/shuili/fragment/securitycloud/BaseSecurityCloudFragment.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -32,7 +33,7 @@ import butterknife.BindView;
 @SuppressLint("ValidFragment")
 public class BaseSecurityCloudFragment extends BaseFragment implements AppBarLayout.OnOffsetChangedListener {
     public static final String TAG = "BaseSecurityCloudFragment";
-    String strJsonData ;//= "{\"accidentInfoEntry\":{\"accLevelFourCount\":0,\"accLevelOneCount\":0,\"accLevelThreeCount\":0,\"accLevelTwoCount\":0,\"deathCount\":0,\"score\":0,\"totalCount\":0},\"compScoreTrend\":{\"dataList\":[{\"date\":null,\"score\":0}],\"qualifiedScore\":0},\"hiddenInfoEntry\":{\"majorHadSupervisingCount\":0,\"majorHiddenCount\":0,\"majorHiddenHadRectifyCount\":0,\"majorHiddenNoRectifyCount\":0,\"majorLateNoRectifyCount\":0,\"noRectifyCount\":0,\"normalHiddenCount\":0,\"normalHiddenHadRectifyCount\":0,\"normalHiddenNoRectifyCount\":0,\"normalLateNoRectifyCount\":0,\"score\":0,\"totalHiddenCount\":0},\"rankList\":[{\"id\":null,\"name\":null,\"score\":0}],\"riskSourceEntry\":{\"hadControl\":0,\"hadRecord\":0,\"noControl\":0,\"noRecord\":0,\"score\":0},\"straightTubeManageEntry\":{\"dataList\":[{\"partReportCount\":0,\"partUnReportCount\":0}],\"perTrainingHours\":0,\"score\":0,\"trainingPersonCount\":0},\"supervisionMangeEntry\":{\"score\":0,\"standardLevelOneCount\":0,\"standardLevelThreeCount\":0,\"standardLevelTwoCount\":0,\"workAssessmentScore\":0},\"synthesisInfoEntry\":{\"chainRatio\":null,\"sameTimeRatio\":null,\"score\":0}}";
+    String strJsonData;//= "{\"accidentInfoEntry\":{\"accLevelFourCount\":0,\"accLevelOneCount\":0,\"accLevelThreeCount\":0,\"accLevelTwoCount\":0,\"deathCount\":0,\"score\":0,\"totalCount\":0},\"compScoreTrend\":{\"dataList\":[{\"date\":null,\"score\":0}],\"qualifiedScore\":0},\"hiddenInfoEntry\":{\"majorHadSupervisingCount\":0,\"majorHiddenCount\":0,\"majorHiddenHadRectifyCount\":0,\"majorHiddenNoRectifyCount\":0,\"majorLateNoRectifyCount\":0,\"noRectifyCount\":0,\"normalHiddenCount\":0,\"normalHiddenHadRectifyCount\":0,\"normalHiddenNoRectifyCount\":0,\"normalLateNoRectifyCount\":0,\"score\":0,\"totalHiddenCount\":0},\"rankList\":[{\"id\":null,\"name\":null,\"score\":0}],\"riskSourceEntry\":{\"hadControl\":0,\"hadRecord\":0,\"noControl\":0,\"noRecord\":0,\"score\":0},\"straightTubeManageEntry\":{\"dataList\":[{\"partReportCount\":0,\"partUnReportCount\":0}],\"perTrainingHours\":0,\"score\":0,\"trainingPersonCount\":0},\"supervisionMangeEntry\":{\"score\":0,\"standardLevelOneCount\":0,\"standardLevelThreeCount\":0,\"standardLevelTwoCount\":0,\"workAssessmentScore\":0},\"synthesisInfoEntry\":{\"chainRatio\":null,\"sameTimeRatio\":null,\"score\":0}}";
     @BindView(R.id.app_bar)
     AppBarLayout appBarLayout;
     @BindView(R.id.collapse_toolbar)
@@ -61,14 +62,11 @@ public class BaseSecurityCloudFragment extends BaseFragment implements AppBarLay
         if (strJsonData != null) {
             securityCloudEntry = gson.fromJson(strJsonData, SecurityCloudEntry.class);
         }
-        int score=securityCloudEntry.getSynthesisInfoEntry().getScore();
-        titleDetail = new String(new StringBuilder(title)
-                .append("·安全评分·")
-                .append(score)
-                .append("分"));
+        int score = securityCloudEntry.getSynthesisInfoEntry().getScore();
+        titleDetail = new String(new StringBuilder(title).append("·安全评分·").append(score).append("分"));
         tvTitle.setText(title);
         collapsingToolbarLayout.setTitle(titleDetail);
-        tvScore.setText(score+"");
+        tvScore.setText(score + "");
         viewDialPlate.updateData(score);
 
         initCharView();
@@ -100,31 +98,53 @@ public class BaseSecurityCloudFragment extends BaseFragment implements AppBarLay
     protected void initListener() {
 
     }
-    private void initCharView(){
-        List<PieEntry> strings = new ArrayList<>();
-        strings.add(new PieEntry(30f,"aaa"));
-        strings.add(new PieEntry(70f,"bbb"));
-        PieDataSet dataSet = new PieDataSet(strings,"Label");
 
+    private void initCharView() {
+        List<PieEntry> strings = new ArrayList<>();
+        strings.add(new PieEntry(11f, "一般事故 11"));
+        strings.add(new PieEntry(4f, "较大事故 4"));
+        strings.add(new PieEntry(3f, "重大事故 3"));
+        strings.add(new PieEntry(2f, "特大事故 2"));
+
+        PieDataSet dataSet = new PieDataSet(strings, "Label");
         ArrayList<Integer> colors = new ArrayList<Integer>();
-        colors.add(getResources().getColor(R.color.yellow));
-        colors.add(getResources().getColor(R.color.blue));
+        colors.add(getResources().getColor(R.color.pie_chart_color_level_one));
+        colors.add(getResources().getColor(R.color.pie_chart_color_level_two));
+        colors.add(getResources().getColor(R.color.pie_chart_color_level_three));
+        colors.add(getResources().getColor(R.color.pie_chart_color_level_four));
         dataSet.setColors(colors);
 
         PieData pieData = new PieData(dataSet);
-        pieData.setDrawValues(true);
-        pieData.setValueFormatter(new PercentFormatter());
-        pieData.setValueTextSize(12f);
+        pieData.setDrawValues(false);
 
-        pieChart.setData(pieData);
-        pieChart.invalidate();
+//        pieData.setValueFormatter(new PercentFormatter());
+//        pieData.setValueTextSize(12f);
 
-        Description description = new Description();
-        description.setText("description");
+        Legend legend = pieChart.getLegend();
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
+        legend.setTextSize(14);
+        legend.setTextColor(getResources().getColor(R.color.text_gray_color));
+//        legend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_CENTER);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        legend.setXOffset(40);
+        legend.setFormSize(14);
+        legend.setFormToTextSpace(12);
+        legend.setYOffset(34);
+//        pieChart.invalidate();
+//        是否允许点击
+        pieChart.setTouchEnabled(false);
+//        是否允许旋转
+        pieChart.setRotationEnabled(false);
+        Description description=new Description();
+        description.setText("");
         pieChart.setDescription(description);
-        pieChart.setHoleRadius(0f);
-        pieChart.setTransparentCircleRadius(0f);
+//        pieChart.setHoleRadius(0.8f);
+        pieChart.setTransparentCircleRadius(55f);
+        pieChart.setData(pieData);
+        pieChart.setNoDataText("暂无数据");
+
     }
+
     @Override
     protected void initData() {
         initViewData();
