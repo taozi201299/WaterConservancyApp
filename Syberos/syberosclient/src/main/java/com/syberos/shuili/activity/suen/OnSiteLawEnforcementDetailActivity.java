ec60b73b1fc1aa8ebf2bed8d43289d07c974fbf1
unknown <my_bills@126.com>
2018-09-27 12:43:03 +0800

    for layout

--- Syberos/syberosclient/src/main/java/com/syberos/shuili/activity/suen/OnSiteLawEnforcementDetailActivity.java
+++ Syberos/syberosclient/src/main/java/com/syberos/shuili/activity/suen/OnSiteLawEnforcementDetailActivity.java
@@ -1,5 +1,9 @@
 package com.syberos.shuili.activity.suen;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.syberos.shuili.R;
 import com.syberos.shuili.base.BaseActivity;
 import com.syberos.shuili.entity.objCase.LawEnforcementEvidenceInformation;
@@ -8,17 +12,14 @@ import com.syberos.shuili.utils.Strings;
 import com.syberos.shuili.view.AudioEditView;
 import com.syberos.shuili.view.MultimediaView;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OnSiteLawEnforcementDetailActivity extends BaseActivity {
+    @BindView(R.id.ll_multimedia)
+    MultimediaView llMultimedia;
 
 //    private final static int REQUEST_CODE = 1545;
 
@@ -70,24 +71,32 @@ public class OnSiteLawEnforcementDetailActivity extends BaseActivity {
                 OnSiteLawEnforcementListActivity.SEND_BUNDLE_KEY);
 
         if (null != lawEnforcementInformation) {
-            String caseName =  lawEnforcementInformation.caseName;
+            String caseName = lawEnforcementInformation.caseName;
             String caseLitiName = lawEnforcementInformation.caseLitiName;
             String filiTime = lawEnforcementInformation.filiTime;
             String coltra1 = lawEnforcementInformation.contra1;
             String coltra2 = lawEnforcementInformation.contra2;
-            setActionBarTitle(caseName == null ?"未知":caseName);
+            setActionBarTitle(caseName == null ? "未知" : caseName);
             tv_name.setText(lawEnforcementInformation.caseName);
-            tv_litigant.setText(caseLitiName == null ?"":caseLitiName);
-            tv_time.setText(filiTime == null ?"":filiTime);
-            if(coltra1 == null) coltra1 = "";
-            if(coltra2 == null) coltra2 = "";
-            tv_undertaker.setText(lawEnforcementInformation.contra1 + "、"+ lawEnforcementInformation.contra2);
+            tv_litigant.setText(caseLitiName == null ? "" : caseLitiName);
+            tv_time.setText(filiTime == null ? "" : filiTime);
+            if (coltra1 == null) coltra1 = "";
+            if (coltra2 == null) coltra2 = "";
+            tv_undertaker.setText(lawEnforcementInformation.contra1 + "、" + lawEnforcementInformation.contra2);
 
             ae_describe_audio.setModel(MultimediaView.RunningMode.READ_ONLY_MODE);
             ae_describe_audio.setLabelText("案件基本情况");
             ae_describe_audio.setEditText(lawEnforcementInformation.caseSitu);
+            llMultimedia.setRunningMode(MultimediaView.RunningMode.READ_ONLY_MODE);
 
         }
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
